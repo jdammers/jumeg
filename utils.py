9a4f0371fcae0fe02b03821828acc50e9d08ecc3
prav <pravsripad@gmail.com>
2014-04-29 10:30:10 +0200

    Fixes.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import mne, sys
 import numpy as np
+import scipy as sci
 
 def mark_bads_batch(subject_list, subjects_dir=None):
     '''
@@ -12,7 +13,7 @@ def mark_bads_batch(subject_list, subjects_dir=None):
 
     Output
     ------
-    The raw files with bads marked are saved with _bads_marked.fif suffix. 
+    The raw files with bads marked are saved with _bcc (for bad channels checked) added to the file name. 
     '''
     for subj in subjects:
         print "For subject %s"%(subj)
@@ -24,49 +25,52 @@ def mark_bads_batch(subject_list, subjects_dir=None):
                 raw = mne.fiff.Raw(dirname+'/'+raw_fname)
                 raw.plot(block=True)
                 print 'The bad channels marked are'+raw.info['bads']
-                raw.save(raw.info['filename'].split('/')[-1].split('.')[0]+'_bads_marked.fif')
+                raw.save(raw.info['filename'].split('/')[-1].split('.')[0]+'_bcc-raw.fif')
                 return
 
 def rescale_artifact_to_signal(signal, artifact):
-    ''' Rescales artifact (ECG/EOG) to signal for plotting purposes '''
+    ''' 
+    Rescales artifact (ECG/EOG) to signal for plotting purposes 
+    For evoked data, pass signal.data.mean(axis=0) and artifact.data.mean(axis=0).
+    '''
     b = (signal.max() - signal.min()) / (artifact.max() + artifact.min())
     a = signal.max()
     rescaled_artifact = artifact * b + a
     return rescaled_artifact / 1e15
 
-def rescale_evoked_artifact(evoked_meg_signal, evoked_artifact):
-    """ Function to rescale an artifact evoked object to MEG evoked for display purposes. Only for grad. """
-    b = (evoked_meg_signal.data.mean(axis=0).max() - evoked_meg_signal.data.mean(axis=0).min()) / \
-        (evoked_artifact.max() + evoked_artifact.min())
-    a = evoked_meg_signal.data.mean(axis=0).max() * 1e15
-    rescaled_artifact = evoked_artifact * b * 1e15 + a
-    return rescaled_artifact
-
 def peak_counter(signal):
-    ''' Simple peak counter '''
-    count = 0
-    thresh = signal.mean() + (signal.max() - signal.mean()) / 2
-    for i in signal:
-        if i > thresh:
-            count += 1
-    return count
+    ''' Simple peak counter using scipy argrelmax function. '''
+    return sci.signal.argrelmax(signal)[0].shape
 
 def update_description(raw, comment):
     ''' Updates the raw description with the comment provided. '''
-    raw.info['description'] = raw.info['description'] + ' \n ' + comment
+    raw.info['description'] = str(raw.info['description']) + ' ; ' + comment
 
-def extract_five_mins_raw_data(raw):
-    ''' This function extracts five mins of raw data excluding the first minute. '''
-    # Check if data is longer than 6 mins. 
-    if (raw.n_times / (raw.info['sfreq'] * 60)) < 6.0:
-        print "The data is not long enough. Please provide a data longer than 6 minutes."
+def chop_raw_data(raw, start_time=60.0, stop_time=360.0):
+    ''' 
+    This function extracts specified duration of raw data and write it into a fif file.
+    Five mins of data will be extracted by default.
+
+    Parameters
+    ----------
+
+    raw: Raw object. 
+    start_time: Time to extract data from in seconds. Default is 60.0 seconds. 
+    stop_time: Time up to which data is to be extracted. Default is 360.0 seconds.
+
+    '''
+    # Check if data is longer than required chop duration.
+    if (raw.n_times / (raw.info['sfreq'])) < (stop_time + 60.0):
+        print "The data is not long enough."
         return
-    # Obtain indexes for the 60th and 360th second.
-    start_time = raw.time_as_index(60.0)
-    stop_time = raw.time_as_index(360.0)
-    data, times = raw[:, start_time:stop_time]
+    # Obtain indexes for start and stop times.
+    assert start_time < stop_time, "Start time is greater than stop time."
+    start_idx = raw.time_as_index(start_time)
+    stop_idx = raw.time_as_index(stop_time)
+    data, times = raw[:, start_idx:stop_idx]
     raw._data,raw._times = data, times
-    raw.save(raw.info['filename'].split('/')[-1].split('.')[0]+'_5m.fif')
+    dur = int((stop_time - start_time) / 60)
+    raw.save(raw.info['filename'].split('/')[-1].split('.')[0]+'_'+str(dur)+'m.fif')
     return
 
 def make_surrogates_sklearn(epochs):
