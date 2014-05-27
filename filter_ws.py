1c997a3f4c0ea9403b43bb895dd8fd35342bed3a
fboers <f.boers@fz-juelich.de>
2014-05-27 16:50:08 +0200

    v0.006 changes to filter_ws

--- filter_ws.py
+++ filter_ws.py
@@ -1,230 +1,263 @@
 import numpy as np
 '''
-#---------------------------------------------------------# 
-#--- Filter_WS             -------------------------#
-#---------------------------------------------------------# 
+#----------------------------------------------------------------------# 
+#--- Filter_WS             --------------------------------------------#
+#----------------------------------------------------------------------# 
 # autor      : Frank Boers 
+# email      : f.boers@fz-juelich.de
 # last update: 26.05.2014
-# version    : 0.005
-#---------------------------------------------------------# 
+# version    : 0.006
+#----------------------------------------------------------------------# 
 # Taken from:
 # The Scientist and Engineer's Guide to Digital Signal Processing
 # By Steven W. Smith, Ph.D.
 # Chapter 16: Window-Sinc Filter
 # http://www.dspguide.com/ch16.htm
-#---------------------------------------------------------# 
+#----------------------------------------------------------------------#
+# the functions 
+# calc_window_hamming, calc_window_blackmann and calc_sinc_filter_data
+# are copied from
+# https://github.com/
+# mubeta06/python/blob/master/signal_processing/sp/firwin.py
+#
+#----------------------------------------------------------------------# 
 #
 # from filter_ws import Filter_WS
 #
-# filter_type = 'bp'
-# fcut1       = 1.0
-# fcut2       = 45.0
-# sampling_frequency = 1017.25
-#
 # fiws = Filter_WS()
-#--- parameter
-# fiws.sampling_frequency = sampling_frequency
-# fiws.filter_type        = filter_type  # bp hp lp notch
-# fiws.fcut1              = fcut1
-# fiws.fcut2              = fcut2
+#
+# fiws = Filter_WS(filter_type='bp',fcut1=11.0,fcut2=35.0,dcoffset=True,sampling_frequency=1234)
+#
+#--- get/set parameter 
+# fiws.sampling_frequency = 1017.25 # e.g. MEG 4D Neuroimaging
+# fiws.filter_type        = 'bp'    # bp hp lp notch
+# fiws.fcut1              = 1
+# fiws.fcut2              = 45
+# fiws.dcoffset           = True    # True for DC correction or False
+#
+#--- init functions 
 #
 # fiws.init_filter_kernel(signal.size)
 # fiws.init_filter(signal.size)
-# fiws.dcoffset           = True or False
 #
-#--- filter run 
+#--- call to apply the filter
+# 
 #   fiws.apply_filter(signal)
-#--> this works inplace input data will be change
-'''
+#--> this works inplace => input data will be change
+#
+#----------------------------------------------------------------------#
+# have fun
+#
+#'''
 
 
 class Filter_WS:
-     def __init__ (self):
-         self._sampling_frequency          = 1017.25
-         self._filter_type                 = 'bp' #lp, bp, hp
-         self._filter_attenuation_factor   = 1.0  # 1, 2
+     def __init__ (self,filter_type='bp', fcut1=1.0, fcut2=45.0, dcoffset=True, sampling_frequency=1017.25 ):
+		 
+         self._sampling_frequency          = sampling_frequency
+         self._filter_type                 = filter_type #lp, bp, hp
+         self._filter_attenuation_factor   = 1  # 1, 2
          self._filter_window               = 'blackmann' # hamming, blackmann
-         self._fcut1                       = 1
-         self._fcut2                       = 200
+         self._fcut1                       = fcut1
+         self._fcut2                       = fcut2
+         self._dcoffset                    = dcoffset
 #---
          self._filter_kernel_length_factor = 16.0
          self._settling_time_factor        = 5.0
          self._data_mean                   = 0.0
-         self._dcoffset                    = 0
+  
 
 
 #--- dcoffset    
-     def set_dcoffset(self,value):
+     def _set_dcoffset(self,value):
          self._dcoffset = value
 
-     def get_dcoffset(self):
+     def _get_dcoffset(self):
          return self._dcoffset
        
-     dcoffset = property(get_dcoffset, set_dcoffset)
+     dcoffset = property(_get_dcoffset, _set_dcoffset)
                        
 #---  settling_time_factor   
-     def set_settling_time_factor(self,value):
+     def _set_settling_time_factor(self,value):
          self._settling_time_factor = value
 
-     def get_settling_time_factor(self):
+     def _get_settling_time_factor(self):
          return self._settling_time_factor
 
-     settling_time_factor = property(get_settling_time_factor, set_settling_time_factor)
+     settling_time_factor = property(_get_settling_time_factor,_set_settling_time_factor)
 
 #--- sampling_frequency  
-     def set_sampling_frequency(self, value):
+     def _set_sampling_frequency(self, value):
          self._sampling_frequency = value
 
-     def get_sampling_frequency(self):
+     def _get_sampling_frequency(self):
          return self._sampling_frequency
 
-     sampling_frequency = property(get_sampling_frequency, set_sampling_frequency)
+     sampling_frequency = property(_get_sampling_frequency,_set_sampling_frequency)
 
 #--- filter_kernel_length_factor    
-     def set_filter_kernel_length_factor(self, value):
+     def _set_filter_kernel_length_factor(self, value):
          self._filter_kernel_length_factor = value
 
-     def get_filter_kernel_length_factor(self):
+     def _get_filter_kernel_length_factor(self):
          return self._filter_kernel_length_factor
 
-     filter_kernel_length_factor = property(get_filter_kernel_length_factor, set_filter_kernel_length_factor)
+     filter_kernel_length_factor = property(_get_filter_kernel_length_factor,_set_filter_kernel_length_factor)
 
 #--- filter_kernel_length
-     def get_filter_kernel_length(self):
+     def _get_filter_kernel_length(self):
          try:
             self._filter_kernel_length = self.sampling_frequency * self.filter_kernel_length_factor
          finally: 
                return self._filter_kernel_length
 
-     filter_kernel_length = property(get_filter_kernel_length)
+     filter_kernel_length = property(_get_filter_kernel_length)
 
 #--- filter_type    
-     def set_filter_type(self,value):
+     def _set_filter_type(self,value):
          self._filter_type = value
 
-     def get_filter_type(self):
+     def _get_filter_type(self):
          return self._filter_type
        
-     filter_type = property(get_filter_type, set_filter_type)
+     filter_type = property(_get_filter_type, _set_filter_type)
 
 #--- fcut1    
-     def set_fcut1(self,value):
+     def _set_fcut1(self,value):
          self._fcut1 = value
     
-     def get_fcut1(self):
+     def _get_fcut1(self):
          return self._fcut1
     
-     fcut1 = property(get_fcut1, set_fcut1)
+     fcut1 = property(_get_fcut1, _set_fcut1)
 
 #--- fcut2    
-     def set_fcut2(self,value):
+     def _set_fcut2(self,value):
          self._fcut2 = value
     
-     def get_fcut2(self):
+     def _get_fcut2(self):
          return self._fcut2
     
-     fcut2 = property(get_fcut2, set_fcut2)
+     fcut2 = property(_get_fcut2, _set_fcut2)
 
 #--- filter_window   
-     def set_filter_window(self,value):
+     def _set_filter_window(self,value):
          self._filter_window = value
  
-     def get_filter_window(self):
+     def _get_filter_window(self):
          return self._filter_window
    
-     filter_window = property(get_filter_window,set_filter_window)
+     filter_window = property(_get_filter_window,_set_filter_window)
      
 #--- filter_kernel_data   
-     def set_filter_kernel_data(self,d):
+     def _set_filter_kernel_data(self,d):
          self._filter_kernel_data = d
      
-     def get_filter_kernel_data(self):
+     def _get_filter_kernel_data(self):
          return self._filter_kernel_data
    
-     filter_kernel_data = property(get_filter_kernel_data,set_filter_kernel_data)
+     filter_kernel_data = property(_get_filter_kernel_data,_set_filter_kernel_data)
 
 #--- filter_kernel_data_cplx     
-     def get_filter_kernel_data_cplx(self):
+     def _get_filter_kernel_data_cplx(self):
          return self._filter_kernel_data_cplx
 
-     filter_kernel_data_cplx = property(get_filter_kernel_data_cplx)
+     filter_kernel_data_cplx = property(_get_filter_kernel_data_cplx)
 
 #---- filter_data_length
      def calc_filter_data_length(self,dl):
          self._filter_data_length = self.calc_zero_padding( dl + self.filter_kernel_data.size * self.settling_time_factor * 2 +1)
          return self._filter_data_length 
 
-     def get_filter_data_length(self):
+     def _get_filter_data_length(self):
          return self._filter_data_length  
     
-     filter_data_length = property(get_filter_data_length)
+     filter_data_length = property(_get_filter_data_length)
 
 #---- filter_attenuation_factor
-     def set_filter_attenuation_factor(self,value):
+     def _set_filter_attenuation_factor(self,value):
          if value < 2 :
             self._filter_attenuation_factor = 1
          else :
             self._filter_attenuation_factor = 2
            
-     def get_filter_attenuation_factor(self):
+     def _get_filter_attenuation_factor(self):
          return self._filter_attenuation_factor 
   
-     filter_attenuation_factor = property(get_filter_attenuation_factor,set_filter_attenuation_factor)
+     filter_attenuation_factor = property(_get_filter_attenuation_factor,_set_filter_attenuation_factor)
   
 #--- data_mean    
-     def set_data_mean(self,value):
+     def _set_data_mean(self,value):
          self._data_mean = value
     
-     def get_data_mean(self):
+     def _get_data_mean(self):
          return self._data_mean
     
-     data_mean = property(get_data_mean, set_data_mean)
+     data_mean = property(_get_data_mean, _set_data_mean)
 
 #--- data_std    
-     def set_data_std(self,value):
+     def _set_data_std(self,value):
          self._data_std = value
     
-     def get_data_std(self):
+     def _get_data_std(self):
          return self._data_std
     
-     data_std = property(get_data_std, set_data_std)
+     data_std = property(_get_data_std, _set_data_std)
 
 #--- data_plane dummy data container to filter data
-     def set_data_plane(self,value):
+     def _set_data_plane(self,value):
          self._data_plane = value
     
-     def get_data_plane(self):
+     def _get_data_plane(self):
          return self._data_plane
     
-     data_plane = property(get_data_plane, set_data_plane)    
+     data_plane = property(_get_data_plane, _set_data_plane)    
     
 #--- data_plane_in dummy data container to filter data
-     def set_data_plane_data_in(self,value):
+     def _set_data_plane_data_in(self,value):
          self._data_plane_data_in = value
     
-     def get_data_plane_data_in(self):
+     def _get_data_plane_data_in(self):
          return self._data_plane_data_in
     
-     data_plane_data_in = property(get_data_plane_data_in, set_data_plane_data_in)
+     data_plane_data_in = property(_get_data_plane_data_in, _set_data_plane_data_in)
     
 #--- data_plane_out dummy data container to filter data
-     def set_data_plane_data_out(self,value):
+     def _set_data_plane_data_out(self,value):
          self._data_plane_data_out = value
     
-     def get_data_plane_data_out(self):
+     def _get_data_plane_data_out(self):
          return self._data_plane_data_out
     
-     data_plane_data_out = property(get_data_plane_data_out, set_data_plane_data_out)
-    
-     
+     data_plane_data_out = property(_get_data_plane_data_out, _set_data_plane_data_out)
+
+#---------------------------------------------------------# 
+#--- filter_name_extention       -------------------------#
+#---------------------------------------------------------# 
+     def filter_name_extention(self):
+         """Return string with filter parameters for file name extention e.g. postfix."""
+         dstr = "fiws" + self.filter_type + "%d-%dord%d" % (self.fcut1, self.fcut2,self.filter_kernel_length_factor)
+         if ( self.filter_attenuation_factor != 1 ):
+              dstr += "att%d" % (self.filter_attenuation_factor)
+        
+         #if ( self.notches ):
+         #    dstr += "n%d" %( self.number_of_notches )
+   
+         if ( self.dcoffset ):
+             dstr += "dc"
+         print dstr
+         return dstr
+         
 #---------------------------------------------------------# 
 #--- window_hamming              -------------------------#
+#---------------------------------------------------------# 
 #---------------------------------------------------------# 
      def calc_window_hamming(self,M):
          """Return an M + 1 point symmetric hamming window."""
          if M%2:
                 raise Exception('M must be even')
-         return (0.54 - 0.46*np.cos(2*np.pi*np.arange(M + 1)/M))
+            
+         return (0.54 - 0.46*np.cos(2*np.pi*np.arange( M + 1)/M))
          
 #---------------------------------------------------------# 
 #--- window_blackman             -------------------------#
@@ -273,7 +306,6 @@ class Filter_WS:
          """Return data with zero mean."""
          self.calc_data_mean_std(data);
          data -= self.data_mean # ptr to data
-         #return (data - self.data_mean,self.data_mean,self.data_std)  
          return (self.data_mean,self.data_std)  
 
 #---------------------------------------------------------# 
@@ -288,20 +320,25 @@ class Filter_WS:
 #---------------------------------------------------------# 
      def calc_filter_kernel(self,M):
          """calc filter kernel type: bp,lp,hp,notch"""
-
+         M       = int(M)
          Midx    = int( M / 2 );
+         
          dfcut1  = self.fcut1 / self.sampling_frequency
          dfcut2  = self.fcut2 / self.sampling_frequency
-         
+                  
+         print "---> fcut"
+         print  self.fcut1 
+         print  self.fcut2
+         print"-----" 
          #--- M has to bee even !!!
          self.filter_kernel_data       = np.zeros( M + 1 ,np.float64)
          #self.filter_kernel_data.dtype = np.double
          
          if self.filter_type == 'lp':
-           print "lp"  
+           #print "lp"  
            self.filter_kernel_data[:] = self.calc_sinc_filter_function(M, dfcut1)
          elif self.filter_type == 'hp':
-             print "hp"
+             #print "hp"
              if self.fcut1 is None:
                 dfcut1= self.fcut2 / self.sampling_frequency
 
@@ -309,7 +346,7 @@ class Filter_WS:
              self.filter_kernel_data[Midx] += 1.0
  
          elif self.filter_type == 'bp':
-             print "bp"
+             #print "bp"
              if self.fcut1 > self.fcut2:
                dfcut2 = self.fcut1 / self.sampling_frequency
                dfcut1 = self.fcut2 / self.sampling_frequency
@@ -345,16 +382,15 @@ class Filter_WS:
 #---------------------------------------------------------# 
      def calc_filter_kernel_cplx(self,data_size):
          self.calc_filter_data_length(data_size)
-         fkd                                     = np.zeros( self.filter_data_length ) #*1.0
-         fkd.dtype                               = np.double
-         fkd[0: self.filter_kernel_data.size ]   = self.filter_kernel_data.copy()   
+         fkd                                    = np.zeros( self.filter_data_length,np.float64 )
+         fkd[0:self.filter_kernel_data.size ]   = self.filter_kernel_data.copy()   
          
-        #--- factor 2 will give -148 dB 
+        #--- factor 2 => -148 dB 
          if self.filter_attenuation_factor == 2 :
            fkd_cplx = np.fft.rfft( fkd )
            self.filter_kernel_data_cplx = fkd_cplx * fkd_cplx
          else :
-        #--- factor 1 will give -74 dB 
+        #--- factor 1 =>  -74 dB 
            self.filter_kernel_data_cplx = np.fft.rfft( fkd )
                      
          return self.filter_kernel_data_cplx
@@ -375,15 +411,18 @@ class Filter_WS:
          
        #--- init data array for filter
          data_length          = self.calc_filter_data_length( number_of_samples )
-         self.data_plane      = np.zeros( data_length )
-         self.data_plane.dtype= np.double
+         self.data_plane      = np.zeros( data_length,np.float64 )
          
        #--- init part of data array for input data => pointer to part of data_plane        
          data_tsl_start_in       = int( self.filter_kernel_data.size * self.settling_time_factor )
          self.data_plane_data_in = self.data_plane[data_tsl_start_in:data_tsl_start_in+number_of_samples]    
       
        #--- init part of data array for output data due to filter kernel shift  => pointer to part of data_plane        
-         data_tsl_start_out       = int( data_tsl_start_in + np.floor( self.filter_kernel_data.size)/2 ) * self.filter_attenuation_factor
+         if (self.filter_attenuation_factor == 1) :
+            data_tsl_start_out   = int( data_tsl_start_in + np.floor( self.filter_kernel_data.size/2 ) )
+         else :
+			data_tsl_start_out   = int( data_tsl_start_in + np.floor( self.filter_kernel_data.size/2 + self.filter_kernel_data.size / 2 * ( self.filter_attenuation_factor-1 ) ) ) 
+        
          self.data_plane_data_out = self.data_plane[data_tsl_start_out:data_tsl_start_out+number_of_samples]    
          
          #  self.data_plane_post  = self.data_plane[data_tsl_start+number_of_samples,:] 
@@ -400,16 +439,17 @@ class Filter_WS:
                  
          self.data_plane.fill(self.data_std)
 
-    #--- cp data at right place in data plane array
-              
+    #--- copy data at right place in data plane array            
          self.data_plane_data_in[:] = data
-               
+    
+    #--- apply fft to data_plane; multiply with cplx filter kernel; apply ifft to get back to signal space
          self.data_plane[:] = np.fft.irfft( np.fft.rfft( self.data_plane ) * self.filter_kernel_data_cplx )
    
-    #--- cp filtered data at right place in data array !!! M-1 kernel shift !!!
+    #--- copy filtered data back at right place in data array !!! M-1 kernel shift !!!
          data[:] = self.data_plane_data_out
-      
-         if ( self.dcoffset ) : 
+
+    #--- retain dc offset       
+         if ( self.dcoffset == False) : 
             data += dmean
      
          return data
@@ -428,29 +468,4 @@ class Filter_WS:
          #print self.data_plane
 
          return 1
-
-
-          # data_tsl_start = int( self.filter_kernel_data.size * self.settling_time_factor )
-          # data_tsl_end   = data_tsl_start + data.size 
-          # data_idx_a     = int( data_tsl_start + np.floor(self.filter_kernel_data.size/2 ) )
-          # data_length    = self.filter_data_length
-          #--- prepare data  
-          # data_dummy         = np.zeros(data_length) #,float64)
-          # data_dummy.dtype   = np.double
-          # data_dummy[data_tsl_start:data_tsl_start+data.size] += data.copy() 
-
-          # data_dummy[0:data_tsl_start] = data[range(data_tsl_start,0,-1)].copy() 
-          #--- prepare post data  
-          # data_tsl_end_start = data.size-2
-          # data_tsl_end_end   = data_tsl_start # - data_tsl_start
-          # data_offset        = data_dummy[data_tsl_start+data.size:] 
-          # data_offset        = data[range(data_tsl_end_start, data_tsl_end_start - data_offset.size ,-1)].copy() 
-          ## data_dummy         = np.fft.irfft( np.fft.rfft(data_dummy) * self.filter_kernel_data_cplx ).copy()
-
-          #data_dummy =scipy.signal.fftconvolve(data_dummy,self.filter_kernel_data)
-
-          #data              *= 0.0
-          #data              += dmean *1.0
-          #data              += data_dummy[data_idx_a:data_idx_a + data.size] 
-
-
+        
