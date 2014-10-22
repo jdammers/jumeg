e48a3a98a8750398708132a730901d5147ef8d1e
fboers <f.boers@fz-juelich.de>
2014-10-22 16:41:04 +0200

    jumeg_filter minor changes

--- filter/__init__.py
+++ filter/__init__.py
@@ -3,8 +3,8 @@
 #--- part of JuMEG project
 #--- License: Simplified BSD
 
-from . import jumeg_filter
-from . import jumeg_filter_base
-from . import jumeg_filter_bw
-from . import jumeg_filter_ws
-
+from .jumeg_filter import jumeg_filter
+#from . import jumeg_filter_base
+#from . import jumeg_filter_bw
+#from . import jumeg_filter_ws
+#from . import jumeg_filter_mne
