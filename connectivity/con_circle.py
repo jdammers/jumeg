7608b6d0d333675f3cf0bfcec35c32d4a6a38023
Praveen Sripad <pravsripad@gmail.com>
2016-10-12 11:18:06 +0200

    Fix #114. Make mne-0.13 compatible changes.

--- connectivity/con_circle.py
+++ connectivity/con_circle.py
@@ -19,7 +19,6 @@ from mne.viz import circular_layout
 from mne.viz.utils import plt_show
 from mne.viz.circle import _plot_connectivity_circle_onpick
 from mne.externals.six import string_types
-from mne.fixes import tril_indices, normalize_colors
 
 from ..jumeg_utils import get_jumeg_path
 
@@ -178,7 +177,7 @@ def _plot_connectivity_circle(con, node_names, indices=None, n_lines=None,
         if con.shape[0] != n_nodes or con.shape[1] != n_nodes:
             raise ValueError('con has to be 1D or a square matrix')
         # we use the lower-triangular part
-        indices = tril_indices(n_nodes, -1)
+        indices = np.tril_indices(n_nodes, -1)
         con = con[indices]
     else:
         raise ValueError('con has to be 1D or a square matrix')
@@ -342,7 +341,7 @@ def _plot_connectivity_circle(con, node_names, indices=None, n_lines=None,
                   axes=axes)
 
     if colorbar:
-        norm = normalize_colors(vmin=vmin, vmax=vmax)
+        norm = plt.normalize_colors(vmin=vmin, vmax=vmax)
         sm = plt.cm.ScalarMappable(cmap=colormap, norm=norm)
         sm.set_array(np.linspace(vmin, vmax))
         cb = plt.colorbar(sm, ax=axes, use_gridspec=False,
