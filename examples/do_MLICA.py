90ffae26e60876121c1a18574b605ed49936eb84
ckiefer0 <c.m.k@gmx.net>
2018-06-14 12:48:37 +0200

    Added apply_ica_replace_mean_std on filtered and unfiltered data

--- examples/do_MLICA.py
+++ examples/do_MLICA.py
@@ -3,6 +3,8 @@ Compute ICA object based on filtered and downsampled data.
 Identify ECG and EOG artifacts using MLICA and compare
 results to correlation & ctps analysis.
 
+Apply ICA object to filtered and unfiltered data.
+
 Ahmad Hasasneh, Nikolas Kampel, Praveen Sripad, N. Jon Shah, and Jürgen Dammers
 “Deep Learning Approach for Automatic Classification of Ocular and Cardiac
 Artifacts in MEG Data,”
@@ -34,16 +36,20 @@ eog2_ch = 'EOG 002'
 refnotch = [50., 100., 150., 200., 250., 300., 350., 400.]
 
 # example filname
-raw_fname = '/data/megraid21/sripad/cau_fif_data/jumeg_test_data/109925_CAU01A_100715_0842_2_c,rfDC-raw.fif'
+raw_fname = "/data/megraid21/sripad/cau_fif_data/jumeg_test_data/" \
+            "109925_CAU01A_100715_0842_2_c,rfDC-raw.fif"
 
 # load the model for artifact rejection:
-model_name = '/data/megraid26/meg_commons/MLICA/models_MLICA_paper_090418/x_validation_shuffle_v4/split_23/split_23-25-0.939.hdf5'
+model_name = "/data/megraid26/meg_commons/MLICA/models_MLICA_paper_090418/" \
+             "x_validation_shuffle_v4/split_23/split_23-25-0.939.hdf5"
+
 model = load_model(model_name)
 
 # noise reducer
 raw_nr = noise_reducer(raw_fname, reflp=5., return_raw=True)
 
-raw_nr = noise_reducer(raw_fname, raw=raw_nr, refhp=0.1, noiseref=['RFG ...'], return_raw=True)
+raw_nr = noise_reducer(raw_fname, raw=raw_nr, refhp=0.1, noiseref=['RFG ...'],
+                       return_raw=True)
 
 # 50HZ and 60HZ notch filter to remove noise
 raw = noise_reducer(raw_fname, raw=raw_nr, refnotch=refnotch, return_raw=True)
@@ -51,14 +57,16 @@ raw = noise_reducer(raw_fname, raw=raw_nr, refnotch=refnotch, return_raw=True)
 picks = mne.pick_types(raw.info, meg=True, eeg=False, eog=False,
                        stim=False, exclude='bads')
 
-raw.filter(0., 45., picks=picks, filter_length='auto',
-           l_trans_bandwidth='auto', h_trans_bandwidth='auto',
-           n_jobs=njobs, method='fir', phase='zero',
-           fir_window='hamming')
+raw_filtered = raw.copy().filter(0., 45., picks=picks, filter_length='auto',
+                                 l_trans_bandwidth='auto', h_trans_bandwidth='auto',
+                                 n_jobs=njobs, method='fir', phase='zero',
+                                 fir_window='hamming')
 
 # downsample the data to 250 Hz, necessary for the model
-raw_ds = raw.copy().resample(250, npad='auto', window='boxcar', stim_picks=None, n_jobs=njobs, events=None)
+raw_ds = raw_filtered.copy().resample(250, npad='auto', window='boxcar', stim_picks=None,
+                                      n_jobs=njobs, events=None)
 raw_ds_chop = raw_ds.copy().crop(tmin=tmin*4./1000, tmax=tmax*4./1000)  # downsampled raw
+raw_filtered_chop = raw_filtered.copy().crop(tmin=tmin*4./1000, tmax=tmax*4./1000)
 raw_chop = raw.copy().crop(tmin=tmin*4./1000, tmax=tmax*4./1000)
 
 ica = ICA(method='fastica', n_components=n_components, random_state=None,
@@ -87,10 +95,8 @@ print model_scores
 for idx in range(0, len(model_scores)):
     if model_scores[idx][0] > MLICA_threshold:
         bads_MLICA.append(idx)
-
-ica.exclude = bads_MLICA
-
 # visualisation
+# ica.exclude = bads_MLICA
 # ica.plot_sources(raw_ds_chop, block=True)
 
 # compare MLICA to results from correlation and ctps analysis
@@ -98,19 +104,29 @@ ica.exclude = []
 
 print 'Identifying components..'
 # get ECG/EOG related components using JuMEG
-ic_ecg = get_ics_cardiac(raw_chop, ica, flow=flow_ecg, fhigh=fhigh_ecg,
+ic_ecg = get_ics_cardiac(raw_filtered_chop, ica, flow=flow_ecg, fhigh=fhigh_ecg,
                          thresh=ecg_thresh, tmin=-0.5, tmax=0.5,
                          name_ecg=ecg_ch, use_CTPS=True)
-ic_eog = get_ics_ocular(raw_chop, ica, flow=flow_eog, fhigh=fhigh_eog,
+ic_eog = get_ics_ocular(raw_filtered_chop, ica, flow=flow_eog, fhigh=fhigh_eog,
                         thresh=eog_thresh, name_eog_hor=eog1_ch,
                         name_eog_ver=eog2_ch, score_func='pearsonr')
 
 bads_corr_ctps = list(ic_ecg) + list(ic_eog)
 bads_corr_ctps = list(set(bads_corr_ctps))  # remove potential duplicates
 bads_corr_ctps.sort()
-ica.exclude = bads_corr_ctps  # to sort and remove repeats
 # visualisation
+# ica.exclude = bads_corr_ctps
 # ica.plot_sources(raw_chop, block=True)
 
 print 'Bad components from MLICA:', bads_MLICA
-print 'Bad components from correlation & ctps:', ica.exclude
+print 'Bad components from correlation & ctps:', bads_corr_ctps
+
+# apply MLICA result to filtered and unfiltered data
+from jumeg.decompose.ica_replace_mean_std import apply_ica_replace_mean_std
+# exclude bad components identified by MLICA
+ica.exclude = bads_MLICA
+raw_filtered_chop_clean = apply_ica_replace_mean_std(raw_filtered_chop, picks=picks,
+                                                     reject={'mag': 5e-12}, verbose=None)
+
+raw_chop_clean = apply_ica_replace_mean_std(raw_chop, picks=picks, reject={'mag': 5e-12},
+                                            verbose=None)
