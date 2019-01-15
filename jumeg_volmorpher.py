37c9ddacc7751251a071355cf32a46be039624fa
ckiefer0 <c.m.k@gmx.net>
2019-01-15 14:01:28 +0100

    improved conditional statements regarding e_func

--- jumeg_volmorpher.py
+++ jumeg_volmorpher.py
@@ -160,11 +160,11 @@ def auto_match_labels(fname_subj_src, label_dict_subject,
     if e_func == 'balltree':
         err_function = 'BallTree Error Function'
         errfunc = _point_cloud_error_balltree
-    if e_func == 'euclidean':
+    elif e_func == 'euclidean':
         err_function = 'Euclidean Error Function'
         errfunc = _point_cloud_error
-    if e_func is None:
-        print 'No Error Function provided, using BallTree instead'
+    else:
+        print 'No or invalid error function provided, using BallTree instead'
         err_function = 'BallTree Error Function'
         errfunc = _point_cloud_error_balltree
 
