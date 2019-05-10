c2b7a4d9c4d3e2f5e3cf3b2977499a45daeca573
fboers <f.boers@fz-juelich.de>
2019-05-10 15:04:53 +0200

    draft version for preproc pipelines

--- pipelines/0_preprocessing.py
+++ pipelines/0_preprocessing.py
@@ -28,6 +28,9 @@ preprocessing function
 
 0_preprocessing.py -s $JUMEG_PATH_LOCAL_DATA/exp/JUMEGTest/mne -subj 211890,211747 -c fbconfig_file.yaml -log -v -d -r -rec
 
+0_preprocessing.py -s $JUMEG_PATH_LOCAL_DATA/exp/JUMEGTest/mne -subj 211747 -c fbconfig_file.yaml -log -v -d -r -rec --logoverwrite
+
+0_preprocessing.py -s $JUMEG_PATH_LOCAL_DATA/exp/MEG94T/mne -lname=meg94t_meeg.txt -lpath=$JUMEG_PATH_LOCAL_DATA/exp/MEG94T/mne -c fbconfig_file.yaml -log -v -d -r -rec --logoverwrite
 -------------
 
 MAIN
@@ -36,31 +39,33 @@ MAIN
 
 import os,sys,logging
 
-import fb_preproc_utils as utils
-from jumeg.base import jumeg_logger
 
-# from jumeg.base.jumeg_base import jumeg_base as jb
+from jumeg.base import jumeg_logger
+import jumeg.base.pipelines.jumeg_pipelines_utils0 as utils
+from jumeg.base.pipelines.jumeg_pipeline_looper import JuMEG_PipelineLooper
 
 logger = logging.getLogger("root")
 
-__version__= "2019.04.30.001"
+__version__= "2019.05.10.001"
 
 defaults={
           "stage"          : ".",
           "fif_extention"  : ["meeg-raw.fif","rfDC-empty.fif"],
           "config"         : "config_file.yaml",
           "subjects"       : None,
-          "logfile"        : True,
+          "log2file"       : True,
+          "logprefix"      : "preproc0",
+          "logoverwrite"   : False,
           "overwrite"      : False,
           "verbose"        : False,
           "debug"          : False,
-          "recursive"      : False
+          "recursive"      : True
          }
 
 #-----------------------------------------------------------
 #--- apply
 #-----------------------------------------------------------
-def apply(opt=None,defaults=None,logprefix="preproc"):
+def apply(name=None,opt=None,defaults=None,logprefix="preproc"):
     """
      jumeg preprocessing step 1
       noise reducer
@@ -73,60 +78,32 @@ def apply(opt=None,defaults=None,logprefix="preproc"):
     :param logprefix: prefix for logfile e.g. name of script
     :return:
     """
-   #--- init globals
-    config,subjects,stage,file_extention,recursive = utils.init_globals(opt=opt,defaults=defaults)
-    raw_fname = None
-    raw       = None
-    file_idx  = 1
+   #---
+    raw = None
     
-   #--- main for loop do preprocessing
-    for raw_fname,recordings_dir,subject_id in utils.subject_file_list(subjects,stage=stage,file_extention=file_extention,recursive=recursive):
-       #--- setup logfile
-        if opt.logfile:
-           log_name = os.path.splitext(raw_fname)[0]
-           Hlog = jumeg_logger.update_filehandler(logger=logger,path=os.path.join(recordings_dir,"log"),prefix=logprefix,name=log_name)
-           logger.info("  -> writing log to: {}".format(Hlog.filename))
-
-        logger.info("---> Start PreProc Ids: {}\n".format(len(subjects))+
-                    " --> subject id       : {} file number: {}\n".format(subject_id,file_idx)+
-                    "  -> raw file name    : {}\n".format(raw_fname)+
-                    "  -> stage            : {}\n".format(stage)+
-                    "  -> basedir          : {}\n".format( os.getcwd())+
-                    "  -> recordings dir   : {}".format(recordings_dir))
-        
-       #--- call noise reduction
-        cfg = config.get("noise_reducer")
-        if cfg.get("run"):
-           logger.info(" --> preproc noise_reducer: {} / {} raw file: {}".format(file_idx,len(subjects),raw_fname))
-           if utils.check_file_extention(fname=raw_fname,file_extention=cfg.get("file_extention",file_extention) ):
-              raw_fname,raw = utils.apply_noise_reducer(os.path.join(recordings_dir,raw_fname),raw=raw,**cfg)
-   
-       #--- call interploate_bads
-        """
-        cfg = config.get("interpolate_bads")
-        if cfg.get("run"):
-           if utils.check_file_extention(fname=raw_fname,file_extention=cfg.get("file_extention") ):
-              raw_fname,raw = utils.apply_interpolate_bads(raw_fname,raw=raw,**cfg)
-           
-       
-   
-       #--- call filter
-        cfg = config.get("filter")
-        if cfg.get("run"):
-           if check_file_extention(fname=raw_fname,file_extention=cfg.get("file_extention") ):
-              apply_filter(raw_fname,raw=raw,**cfg)
+   #--- init/update logger
+    jumeg_logger.setup_script_logging(name=name,opt=opt,logger=logger)
+ 
+    jpl = JuMEG_PipelineLooper(options=opt,defaults=defaults)
+    
+    for raw_fname,subject_id,dir in jpl.file_list():
       
-       #--- call resample
-        cfg = config.get("resample")
-        if cfg.get("run"):
-           if check_file_extention(fname=raw_fname,file_extention=cfg.get("file_extention") ):
-              apply_resample(raw_fname,raw=raw,**cfg)
-        """
-        file_idx += 1
+       #--- call noise reduction
+        raw_fname,raw = utils.apply_noise_reducer(raw_fname,raw=raw,**jpl.config.get("noise_reducer"))
+    
+       #--- call suggest_bads
+        raw_fname,raw = utils.apply_suggest_bads(raw_fname,raw=raw,**jpl.config.get("suggest_bads"))
+       
+       #--- call interploate_bads
+        raw_fname,raw = utils.apply_interpolate_bads(raw_fname,raw=raw,**jpl.config.get("interpolate_bads") )
         
-        if Hlog:
-           Hlog.close()
-   
+       #--- call filter
+       # raw_fname,raw = utils.apply_filter(raw_fname,raw=raw,**jpl.config.get("filtering") )
+
+       #--- call resample
+       # raw_fname,raw = utils.apply_resample(raw_fname,raw=raw,**jpl.config.get("resampling"))
+
+
 #=========================================================================================
 #==== MAIN
 #=========================================================================================
@@ -137,13 +114,7 @@ def main(argv):
        parser.print_help()
        sys.exit(-1)
        
-  #--- init/update logger
-    jumeg_logger.setup_script_logging(name=argv[0],opt=opt,logger=logger)
-    
-  #--- logfile  prefix
-    pgr_name = os.path.splitext( os.path.basename(argv[0]) )[0]
-    
-    if opt.run: apply(opt=opt,defaults=defaults,logprefix=pgr_name)
+    if opt.run: apply(name=argv[0],opt=opt,defaults=defaults)
     
 if __name__ == "__main__":
    main(sys.argv)
--- pipelines/fb_preproc_utils.py
+++ pipelines/fb_preproc_utils.py
@@ -21,10 +21,11 @@ import sys,os,logging,yaml,argparse,glob
 import mne
 
 from jumeg.base.jumeg_base         import jumeg_base as jb
-from jumeg.base.jumeg_logger       import StreamToLogger # capture stout,stderr
+from jumeg.base                    import jumeg_logger  #     import STDStreamLogger # capture stout,stderr
 from jumeg.plot.jumeg_plot_preproc import JuMEG_PLOT_PSD
-from jumeg.jumeg_noise_reducer     import noise_reducer
 
+#--- preproc
+from jumeg.jumeg_noise_reducer     import noise_reducer
 from jumeg.jumeg_suggest_bads      import suggest_bads
 from jumeg.jumeg_interpolate_bads  import interpolate_bads as jumeg_interpolate_bads
 
@@ -32,125 +33,6 @@ logger = logging.getLogger("root")
 
 __version__= "2019.04.18.001"
 
-
-#---
-def init_globals(opt=None,defaults=None):
-    """
-     init global parameter
-      load config file parameter
-      init <subject list>,<stage>,<fif_extention>  from opt or config or defaults
-      
-    :param opt: arparser option obj
-    :param defaults: default dict
-    
-    :return:
-    config parameter: dict
-    subject ids     : list
-    stage           : start / base dir
-    fif_extention   : list of file extentions, files must match to get started
-    recursive       : option to search for files in subfolder <True/False>
-    """
-    #--- init logging
-    #logger = jumeg_logger.setup_script_logging(opt=opt,level="DEBUG",logfile=False)
-    
-    if not defaults:
-       defaults = {}
-    
-    #--- load cfg ToDo in CLS
-    fcfg = opt.config if opt.config else defaults.get("config")
-    if opt.debug:
-        logger.info("  -> loading config file: {} ...".format(fcfg))
-    with open(fcfg,'r') as f:
-        config = yaml.load(f)
-    if opt.debug:
-        logger.info("  -> DONE loading config file")
-    
-    #--- get global parameter from cfg ToDo in CLS
-    cfg_global = config.get("global")
-    
-    #--- get subjects
-    subjects = opt.subjects.split(",") if opt.subjects else cfg_global.get("subjects",defaults.get("subjects"))
-    
-    #--- get subjects
-    stage = opt.stage if opt.stage else cfg_global.get("stage",defaults.get("stage"))
-    #--- get fif file extentio to start with
-    fif_extention = opt.fif_extention if opt.fif_extention else cfg_global.get("fif_extention",defaults.get("fif_extention"))
-
-    recursive = opt.recursive if opt.recursive else cfg_global.get("recursive",defaults.get("recursive"))
-    
-    return config,subjects,stage,fif_extention,recursive
-
-#---
-def check_file_extention(fname=None,file_extention=None):
-    if not fname:
-        return False
-    if file_extention:
-        if isinstance(file_extention,(list)):
-            if fname.split(",")[-1] in file_extention:
-                return True
-        elif fname.split(",")[-1] == file_extention:
-            return True
-    return False
-
-
-#---
-def subject_file_list(subjects,stage=".",file_extention=None,separator=",",recursive=False):
-    """
-    something like a contexmanager in a loop
-    loop over subjects dir
-    avoid copy/paste and boring repetitions
-    place more error handling and file checking
-    handels system ENVs
-
-    https://stackoverflow.com/questions/29708445/how-do-i-make-a-contextmanager-with-a-loop-inside
-
-    :param subjects       : string or list of strings e.g: subject ids
-    :param stage          : start dir,stage
-    :param file_extention : string or list  <None>
-                            if <file_extention> checks filename extention ends with extentio from list
-    :param separator      : split file to get file extention <,>
-    :param recursive      : recursive searching for files in <stage/subject_id> using glob.iglob <False>
-    :return:
-    fname,recordings_dir,subject id
-
-     Example:
-     --------
-     for raw_fname,recordings_dir,subject_id in subject_file_list(subjects,stage_dir= my_stage):
-         print("---> subject: {}\n  -> dir: {}\n  -> fname: {}".format(subject_id,recordings_dir,raw_fname) )
-    """
-    #--- expand envs
-    start_dir = os.path.expandvars(os.path.expanduser(stage))
-    
-    if not isinstance(subjects,(list)):
-        subjects = [subjects]
-    if not isinstance(file_extention,(list)):
-        file_extention = [file_extention]
-    if recursive:
-       fpatt = '**/*'
-    else:
-       fpatt = '*'
-
-    for subj in subjects:
-        try:
-         #--- check if its a dir
-           if os.path.isdir(os.path.join(start_dir,subj)):
-               recordings_dir = os.path.join(start_dir,subj)
-              
-               with jb.working_directory(recordings_dir):
-                    for fext in file_extention:
-                        #for fname in glob.glob(fpatt + fext,recursive=recursive):
-                        flist = glob.glob(fpatt + fext,recursive=recursive)
-                        flist.sort()
-                        for fname in flist:
-                            dir = os.path.dirname(fname)
-                            if dir == "":
-                               dir = "."
-                            yield os.path.basename(fname),dir,subj
-        except:
-            logger.exception("---> error subject : {}\n".format(subj)+
-                             "  -> recordings dir: {}\n".format(recordings_dir)+
-                             "  -> sub dir       : {}\n".format(dir)+
-                             "  -> file          : {}\n".format(os.path.basename(fname)) )
 
 #---
 def get_args(argv,parser=None,defaults=None):
@@ -176,6 +58,12 @@ def get_args(argv,parser=None,defaults=None):
                   -> start path to directory structure
                   e.g. /data/megstore1/exp/M100/mne/
                   """
+     h_lpath    = "path for list file, list of file to process containing list of full filenames"
+     h_lname    = "list file name, list of file to process containing list of full filenames"
+    
+     h_fpath    = "path for file to process"
+     h_fname    = "file name to process"
+    
     h_fextention ="fif file extention or list of extentions, looking for files to process with these extention e.g. meeg-raw.fif or [meeg-raw.fif,rfDC-empty.fif]"
     
     h_subjects  = "subject id or ids  e.g.: 123 or 234,456"
@@ -197,6 +85,12 @@ def get_args(argv,parser=None,defaults=None):
     parser.add_argument("-subj","--subjects", help=h_subjects)#,default=defaults.get("subjects"))
     parser.add_argument("-fif_ext","--fif_extention",help=h_fextention)#,default=defaults.get("fif_extention") )
    #---
+    parser.add_argument("-lpath","--list_path",help=h_lpath)
+    parser.add_argument("-lname","--list_name",help=h_lname)
+   #---
+    parser.add_argument("-fpath","--fpath",help=h_fpath)
+    parser.add_argument("-fname","--fname",help=h_fname)
+   #---
     parser.add_argument("-c","--config",help=h_config,default=defaults.get("config") )
    #--- flags
     parser.add_argument("-o",  "--overwrite",action="store_true",help=h_overwrite, default=defaults.get("overwrite") )
@@ -205,7 +99,8 @@ def get_args(argv,parser=None,defaults=None):
     parser.add_argument("-rec","--recursive",action="store_true",help="search recursive find files in subfolders",default=defaults.get("recursive",False))
 
     parser.add_argument("-r",  "--run",      action="store_true",help="!!! EXECUTE & RUN this program !!!",default=defaults.get("run",True))
-    parser.add_argument("-log","--logfile",  action="store_true",help="generate logfile",default=defaults.get("logfile"))
+    parser.add_argument("-log","--log2file", action="store_true",help="generate logfile",default=defaults.get("log2file"))
+    parser.add_argument("-logprefix","--logprefix", help="logfile prefix",default= os.path.splitext( os.path.basename(argv[0]) )[0])
   
   #--- init flags
   # ck if flag is set in argv as True
@@ -234,12 +129,15 @@ def get_args(argv,parser=None,defaults=None):
 def apply_noise_reducer(raw_fname,raw=None,**cfg):
     '''
     apply <magic ee noise reducer> thrice to reference channels with different freq parameters
-    
-     !!! overwrite raw-obj, works inplace !!!
-    
+    save PSD plot in subfolder /plots
+   
+    !!! overwrite raw-obj, works inplace !!!
+  
+    0) reset bads and check for dead channels
     1) apply nr low pass filter for freq below e.g.: 5Hz  <reflp>
     2) apply nr high pass filter if defined               <reflp>
     3) apply nr notch filter to remove power line noise   <refnotch>
+    4) save PSD plot
     
     Parameter:
     -----------
@@ -251,9 +149,18 @@ def apply_noise_reducer(raw_fname,raw=None,**cfg):
         reflp      : <None>\n
         refhp      : <None>\n
         refnotch   : <None>\n
-        save       : <None>\n
-        overwrite  : overwrite existing file <True>
+        
+        plot: True
+        plot_show : True
         plot_dir   : subdir to save plots
+       
+        postfix       : "nr"
+        file_extention: ["meeg-raw.fif","rfDC-empty.fif"]
+        
+        run      : True
+        save     : True
+        overwrite: True
+        
     ToDo add parameter extended
     <noise_reducer> parameter extended
        signals=[], noiseref=[], detrending=None,
@@ -268,76 +175,112 @@ def apply_noise_reducer(raw_fname,raw=None,**cfg):
     
    #--- init plot
     jplt = None
-    logger.debug("  ->config parameter:\n{}".format( jb.pp_list2str(cfg) ))
+    logger.debug("  ->noise reducer config parameter:\n{}".format( jb.pp_list2str(cfg) ))
+
+    if not jb.check_file_extention(fname=raw_fname,file_extention=cfg.get("file_extention") ):
+       return
+
+    if not cfg.get("run"):
+     #--- return raw_fname,raw
+       return jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=False,
+                                     postfix = cfg.get("postfix","nr"),overwrite = cfg.get("overwrite",True))
+    
+    logger.info(" --> preproc noise_reducer for raw file: {}".format(raw_fname))
     
    #--- catch stdout,stderr
-    sys.stdout = StreamToLogger(logger,logging.INFO)
-    sys.stderr = StreamToLogger(logger,logging.ERROR)
-    
-    
-    ToDO noise_reducer set  reject in config file
-    
-    #--- fb
-        reject = dict(grad=4000e-13, # T / m (gradiometers)
-                      mag=10e-12,     # T (magnetometers)
-                      eeg=40e-6,     # uV (EEG channels)
-                      eog=250e-6)    # uV (EOG channels)
+    jumeg_logger.log_stdout()  #StreamLoggerSTDOUT(level=logging.DEBUG)
 
-    #--- noise reduction
+    #sys.stdout = jumeg_logger.STDStreamLogger(level=logging.DEBUG)
+    # sys.stderr = jumeg_logger.STDStreamLogger(logger,logging.ERROR)
+
+    
+   #--- noise reduction
     # apply noise reducer thrice to reference channels with different freq parameters
     # !!! overwrite raw-obj !!!
-    
+    save        = False
     raw_changed = False
-
+    
+    jb.verbose = cfg.get("verbose")
+   
    #--- load raw, reset bads
-    jb.verbose = True
-    raw,bads = jb.update_bad_channels(raw_fname,raw=raw,save=True)
-    
-    #raw,raw_fname = jb.get_raw_obj(raw_fname,raw=raw)
-    
-    #--- start plot denoising orig raw psd
-    #    avoid reloading raw data
-    
-    
+    raw,raw_fname = jb.get_raw_obj(raw_fname,raw=raw,reset_bads=True)
+   #--- check dead channes and mark them as bad
+    jb.picks.check_dead_channels(raw=raw)
+  
+   #--- start plot denoising orig raw psd, avoid reloading raw data
     if cfg.get("plot"):
        jplt = JuMEG_PLOT_PSD(n_plots=2,name="denoising",verbose=True)
-       jplt.plot(raw,title="orig: " + os.path.basename(raw_fname) )
-    
-    
-    jb.update_bad_channels(raw_fname,raw=raw,save=True,append=True)
-    
+       jplt.plot(raw,title="orig: " + os.path.basename(raw_fname),check_dead_channels=False)
     
     #--- 1 nr low pass filter for freq below 5 hz
     if cfg.get("reflp"):
        #with redirect_stdout(logger):
-       raw = noise_reducer(None,raw=raw,reflp=cfg.get("reflp"),return_raw=True,verbose=cfg.get("verbose"))
+       raw = noise_reducer(None,raw=raw,reflp=cfg.get("reflp"),return_raw=True,verbose=cfg.get("verbose"),exclude_artifacts=False)
        raw_changed = True
     #--- 2 nr high pass filter
     if cfg.get("refhp"):
        #with redirect_stdout(logger):
-       raw = noise_reducer(None,raw=raw,reflp=cfg.get("refhp"),return_raw=True,verbose=cfg.get("verbose"))
+       raw = noise_reducer(None,raw=raw,reflp=cfg.get("refhp"),return_raw=True,verbose=cfg.get("verbose"),exclude_artifacts=False)
        raw_changed = True
     #--- 3  nr notch filter to remove power line noise
     if cfg.get("refnotch"):
        #with redirect_stdout(logger):
        raw = noise_reducer(None,raw=raw,refnotch=cfg.get("refnotch"),fnout=None,return_raw=True,
-                           verbose=cfg.get("verbose"))
+                           verbose=cfg.get("verbose"),exclude_artifacts=False)
        raw_changed = True
 
    #--- save and update filename in raw
-    if raw_changed:
-       fname_out,raw = jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=cfg.get("save"),
-                                               postfix=cfg.get("postfix","nr"),overwrite=cfg.get("overwrite",True))
+    if cfg.get("save"):
+       save = raw_changed
+    fname_out,raw = jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=save,
+                                           postfix=cfg.get("postfix","nr"),overwrite=cfg.get("overwrite",True))
     
     #--- plot results, avoid reloading raw data
     if cfg.get("plot"):
-       jplt.plot(raw,title="denoised: "+os.path.basename(raw_fname))
-       if cfg.get("show_plot"):
-          jplt.show_plot()
+       jplt.plot(raw,title="denoised: "+os.path.basename(fname_out),check_dead_channels=False)
+       if cfg.get("plot_show"):
+          jplt.show()
        jplt.save(fname=fname_out,plot_dir=cfg.get("plor_dir","plots"))
-    
+  
+   #--- return back stdout/stderr from logger
+    sys.stdout = sys.__stdout__
+   # sys.stderr = sys.__stderr__
+
+    #--- catch stdout,stderr
+    # sys.stdout = StreamToLogger(logger,logging.INFO)
+    #sys.stderr = StreamToLogger(logger,logging.ERROR)
+
+    #--- return back stdout/stderr from logger
+    # sys.stdout = sys.__stdout__
+    # sys.stderr = sys.__stderr__
+
     return fname_out,raw
 
+#---------------------------------------------------
+#--- apply_suggest_bads
+#---------------------------------------------------
+def apply_suggest_bads(raw_fname,raw=None,**cfg):
+    """
+
+    :param raw_fname:
+    :param raw:
+    :param cfg:
+    :return:
+     filename,raw-obj
+    """
+    logger.info("  ->config parameter:\n{}".format(cfg))
+  
+    raw_changed   = False
+    jb.verbose    = cfg.get("verbose")
+    raw,raw_fname = jb.get_raw_obj(raw_fname,raw=raw)
+   #---
+    marked,raw = suggest_bads(raw) #,**cfg["parameter"]) #show_raw=cfg.get("show_raw") )
+   #--- save and update filename in raw
+    if raw_changed:
+        fname_out,raw = jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=cfg.get("save"),
+                                               postfix=cfg.get("postfix","bcc"),overwrite=cfg.get("overwrite",True))
+
+    return fname_out,raw
 
 #---------------------------------------------------
 #--- apply_interpolate_bads
@@ -351,41 +294,23 @@ def apply_interpolate_bads(raw_fname,raw=None,**cfg):
     :return:
      filename,raw-obj
     """
-    logger.debug("  ->config parameter:\n{}".format(cfg))
+    logger.info("  ->config parameter:\n{}".format(cfg))
     
-    #--- interpolate bads
-    # !!! overwrite raw-obj !!!
+    jb.verbose = cfg.get("verbose")
+    raw,raw_fname = jb.get_raw_obj(raw_fname,raw=raw)
     
-    raw_changed = False
-    '''
-    ib_dict = dict()
-    
-    subj = op.basename(raw_fname).split('_')[0]
-    
-    if not op.isfile(bcc_fname):
-        raw = mne.io.Raw(op.join(dirname,raw_fname),preload=True)
-        # automatically suggest bad channels and plot results for visual inspection
-        marked,raw = suggest_bads(raw,show_raw=True)
-        
-        ib_dict['bads_channels'] = raw.info['bads']
-        
-        # Interpolate bad channels using jumeg
-        raw_bcc = jumeg_interpolate_bads(raw,origin=None,reset_bads=True)
-        
-        # check if everything looks good
-        raw_bcc.plot(block=True)
-        raw_bcc.save(bcc_fname,overwrite=True)
-        
-        ib_dict['output_file'] = bcc_fname
-        
-        save_state_space_file(ss_dict_fname,process='interpolate_bads',
-                              input_fname=raw_fname,process_config_dict=ib_dict)
+    #--- Interpolate bad channels using jumeg
+    raw = jumeg_interpolate_bads(raw) #,**cfg.get("parameter"))  #,origin=cfg.get("origin",None),reset_bads=cfg.get("reset_bads",True) )
+    #-- check results
+
+    raw.plot(block=cfg.get("plot_block"))
+    raw_changed = True
     
     #--- save and update filename in raw
     if raw_changed:
         fname_out,raw = jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=cfg.get("save"),
-                                               postfix=cfg.get("postfix","nr"),overwrite=cfg.get("overwrite",True))
-    '''
-   # return fname_out,raw
+                                               postfix=cfg.get("postfix","bcc"),overwrite=cfg.get("overwrite",True))
+    
+    return fname_out,raw
 
 
