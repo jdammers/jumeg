4fa2f40f12940469ae469b28c99eacab04c56a42
jdammers <j.dammers@fz-juelich.de>
2019-10-29 10:37:47 +0100

    slight change in parameters and ensure correct cropping

--- examples/decompose/do_MLICA.py
+++ examples/decompose/do_MLICA.py
@@ -1,7 +1,7 @@
 """
 Compute ICA object based on filtered and downsampled data.
-Identify ECG and EOG artifacts using MLICA and compare
-results to correlation & ctps analysis.
+Identify ECG and EOG artifacts using the pre-trained DCNN model
+and compare results using correlation & ctps analysis.
 
 Apply ICA object to filtered and unfiltered data.
 
@@ -24,12 +24,14 @@ from jumeg.jumeg_preprocessing import get_ics_cardiac, get_ics_ocular
 from jumeg.jumeg_plot import plot_performance_artifact_rejection
 from jumeg.jumeg_utils import get_jumeg_path
 
-# config
-MLICA_threshold = 0.8
-n_components = 60
-njobs = 4  # for downsampling
-tmin = 0
-tmax = tmin + 15000
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# settings
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+model_thresh = 0.8                        # >0.5 ..0.9
+n_components = 40                         # 30 .. 60
+njobs = 4
+sfreq_new = 250                           # downsampling to 250 Hz
+flow_raw, flow_high = 2, 45               # high pass filter prevents from false positives
 flow_ecg, fhigh_ecg = 8, 20
 flow_eog, fhigh_eog = 1, 20
 ecg_thresh, eog_thresh = 0.3, 0.3
@@ -39,76 +41,97 @@ eog2_ch = 'EOG 002'
 reject = {'mag': 5e-12}
 refnotch = [50., 100., 150., 200., 250., 300., 350., 400.]
 
-data_path = op.join(get_jumeg_path(), 'data')
-print(data_path)
+# number time samples is fixed to 15000
+nsamples_chop = 15000
+ix_t1 = 0                              # time index: here we use the first chop
+ix_t2 = ix_t1 + nsamples_chop
 
-# example filname
-raw_fname = "/Volumes/megraid21/sripad/cau_fif_data/jumeg_test_data/" \
-            "109925_CAU01A_100715_0842_2_c,rfDC-raw.fif"
-
-# load the model for artifact rejection
-# the details of the model is provided in the x_validation_shuffle_v4_split_23.txt
-model_name = op.join(data_path, "dcnn_model.hdf5")
-
+# ----------------------------------------------
+# load DCNN model for artifact rejection
+# the details of the model is provided in:
+#       x_validation_shuffle_v4_split_23.txt
+# model was trained on 4D data from Juelich
+# ----------------------------------------------
+model_path = op.join(get_jumeg_path(), 'data')
+model_name = op.join(model_path, "dcnn_model.hdf5")
 model = load_model(model_name)
 
-# noise reducer
-raw_nr = noise_reducer(raw_fname, reflp=5., return_raw=True)
-
-raw_nr = noise_reducer(raw_fname, raw=raw_nr, refhp=0.1, noiseref=['RFG ...'],
-                       return_raw=True)
-
-# 50HZ and 60HZ notch filter to remove noise
-raw = noise_reducer(raw_fname, raw=raw_nr, refnotch=refnotch, return_raw=True)
-
+# ----------------------------------------------
+# read example data file
+# ----------------------------------------------
+path_data = '/data/megraid22/Common/DeepLearning/cau_data_validation/'
+raw_fname = path_data + '109925_CAU01A_100715_0842_2_c,rfDC,t1,n_bcc,nr-raw.fif'
+raw = mne.io.Raw(raw_fname, preload=True)
 picks = mne.pick_types(raw.info, meg=True, eeg=False, eog=False,
                        stim=False, exclude='bads')
 
-raw_filtered = raw.copy().filter(0., 45., picks=picks, filter_length='auto',
+# ----------------------------------------------
+# noise reducer
+# ----------------------------------------------
+raw_nr = noise_reducer(raw_fname, reflp=5., return_raw=True)
+raw_nr = noise_reducer(raw_fname, raw=raw_nr, refhp=0.1, noiseref=['RFG ...'],
+                       return_raw=True)
+raw = noise_reducer(raw_fname, raw=raw_nr, refnotch=refnotch, return_raw=True)
+
+# ----------------------------------------------
+# filtering and down sampling
+# ----------------------------------------------
+# filter prior to ICA
+raw_filtered = raw.copy().filter(flow_raw, flow_high, picks=picks, filter_length='auto',
                                  l_trans_bandwidth='auto', h_trans_bandwidth='auto',
-                                 n_jobs=njobs, method='fir', phase='zero',
+                                 n_jobs='cuda', method='fir', phase='zero',
                                  fir_window='hamming')
+# downsample data
+raw_ds = raw_filtered.copy().resample(sfreq_new, npad='auto', window='boxcar', stim_picks=None,
+                                      n_jobs='cuda', events=None)
 
-# downsample the data to 250 Hz, necessary for the model
-raw_ds = raw_filtered.copy().resample(250, npad='auto', window='boxcar', stim_picks=None,
-                                      n_jobs=njobs, events=None)
-raw_ds_chop = raw_ds.copy().crop(tmin=tmin*4./1000, tmax=tmax*4./1000)  # downsampled raw
-raw_filtered_chop = raw_filtered.copy().crop(tmin=tmin*4./1000, tmax=tmax*4./1000)
-raw_chop = raw.copy().crop(tmin=tmin*4./1000, tmax=tmax*4./1000)
+# ----------------------------------------------
+# crop data to get first chop
+# ----------------------------------------------
+# get times to crop
+dt = raw_ds.times[1]             # time period between two time samples
+tmin = ix_t1 * dt
+tmax = ix_t2 * dt - dt           #  subtract one sample
+# crop all raw objects
+raw_chop = raw.copy().crop(tmin=tmin, tmax=tmax)                     # raw
+raw_filtered_chop = raw_filtered.copy().crop(tmin=tmin, tmax=tmax)   # raw filtered
+raw_ds_chop = raw_ds.copy().crop(tmin=tmin, tmax=tmax)               # raw filtered downsampled
 
+# ----------------------------------------------
+# apply ICA
+# ----------------------------------------------
 ica = ICA(method='fastica', n_components=n_components, random_state=42,
           max_pca_components=None, max_iter=5000, verbose=None)
-
 # do the ICA decomposition on downsampled raw
 ica.fit(raw_ds_chop, picks=picks, reject=reject, verbose=None)
+sources = ica.get_sources(raw_ds_chop)._data                     # get sources
+sources = np.reshape(sources, (n_components,nsamples_chop, 1))   # reshape sources
 
-sources = ica.get_sources(raw_ds_chop)._data
-
-# extract temporal and spatial components
-mm = np.float32(np.dot(ica.mixing_matrix_[:, :].T,
+# ----------------------------------------------
+# model prediction
+# identification of artifact components
+# ----------------------------------------------
+# compute base functions
+mm = np.float32(np.dot(ica.mixing_matrix_[:, :ica.n_components_].T,
                        ica.pca_components_[:ica.n_components_]))
+# get model prediction
+model_scores = model.predict([mm, sources], verbose=1)
+# get ICs
+bads_MLICA = list(np.where(model_scores[:,0] > model_thresh)[0])
 
-# use [:, :15000] to make sure it's 15000 data points
-chop = sources[:, :15000]
-chop_reshaped = np.reshape(chop, (len(chop), len(chop[0]), 1))
+# ----------------------------------------------
+# order ICs for visualization
+# ----------------------------------------------
+var_order = sources.std(axis=1).flatten().argsort()[::-1]
+good_ics = np.setdiff1d(var_order, bads_MLICA)
+ic_order = list(np.concatenate([bads_MLICA, good_ics]))
+# store components in ica object
+ica.exclude = list(bads_MLICA)
 
-model_scores = model.predict([mm, chop_reshaped], verbose=1)
 
-bads_MLICA = []
-
-# print model_scores
-
-for idx in range(0, len(model_scores)):
-    if model_scores[idx][0] > MLICA_threshold:
-        bads_MLICA.append(idx)
-
-# visualisation
-# ica.exclude = bads_MLICA
-# ica.plot_sources(raw_ds_chop, block=True)
-
-# compare MLICA to results from correlation and ctps analysis
-ica.exclude = []
-
+# ----------------------------------------------
+# compare MLICA results with correlation and ctps
+# ----------------------------------------------
 print('Identifying components..')
 # get ECG/EOG related components using JuMEG
 ic_ecg = get_ics_cardiac(raw_filtered_chop, ica, flow=flow_ecg, fhigh=fhigh_ecg,
@@ -117,22 +140,21 @@ ic_ecg = get_ics_cardiac(raw_filtered_chop, ica, flow=flow_ecg, fhigh=fhigh_ecg,
 ic_eog = get_ics_ocular(raw_filtered_chop, ica, flow=flow_eog, fhigh=fhigh_eog,
                         thresh=eog_thresh, name_eog_hor=eog1_ch,
                         name_eog_ver=eog2_ch, score_func='pearsonr')
-
 bads_corr_ctps = list(ic_ecg) + list(ic_eog)
 bads_corr_ctps = list(set(bads_corr_ctps))  # remove potential duplicates
 bads_corr_ctps.sort()
-
-# visualisation
-# ica.exclude = bads_corr_ctps
-# ica.plot_sources(raw_chop, block=True)
-
 print('Bad components from MLICA:', bads_MLICA)
 print('Bad components from correlation & ctps:', bads_corr_ctps)
 
-# apply MLICA result to filtered and unfiltered data
-# exclude bad components identified by MLICA
-ica.exclude = bads_MLICA
 
+# ----------------------------------------------
+# plot results
+# ----------------------------------------------
+# plot sources
+fig = ica.plot_sources(raw_filtered_chop, picks=ic_order, title='MLICA', show=False)
+#fig.savefig('MLICA_ica-sources.png')
+
+# plot artifact rejection performance
 fnout_fig = '109925_CAU01A_100715_0842_2_c,rfDC,0-45hz,ar-perf'
 ica_filtered_chop = ica_update_mean_std(raw_filtered_chop, ica, picks=picks, reject=reject)
 raw_filtered_chop_clean = ica_filtered_chop.apply(raw_filtered_chop, exclude=ica.exclude,
@@ -142,10 +164,10 @@ ica_unfiltered_chop = ica_update_mean_std(raw_chop, ica, picks=picks, reject=rej
 raw_unfiltered_chop_clean = ica_unfiltered_chop.apply(raw_chop, exclude=ica.exclude, n_pca_components=None)
 
 # create copy of original data since apply_ica_replace_mean_std changes the input data in place (raw and ica)
-raw_copy = raw.copy().crop(tmin=tmin*4./1000, tmax=tmax*4./1000)
+raw_copy = raw.copy().crop(tmin=tmin, tmax=tmax)
 
 plot_performance_artifact_rejection(raw_copy, ica_unfiltered_chop, fnout_fig,
                                     meg_clean=raw_unfiltered_chop_clean,
-                                    show=False, verbose=False,
+                                    show=True, verbose=False,
                                     name_ecg=ecg_ch,
                                     name_eog=eog2_ch)
